871286a190cffcddd2c3b8afbf6d1df39e856e18
Vinícius <marcos.vinicius912142@outlook.com>
2023-11-28 20:15:08 -0300

    add to cart feedback

--- src/pages/Cardapio.jsx
+++ src/pages/Cardapio.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import {useState} from 'react';
 import { Popover, Button, Typography } from '@mui/material';
 import { CardapioStyles } from '../styles/Cardapio';
 import {useAtom} from 'jotai';
 import { writeOnlyAtom } from '../store/cart';
+import { Snackbar, Alert } from '@mui/material';
 
 function Cardapio({menus}) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const[openSnackbar, setOpenSnackbar]=useState(false)
   const [_, addToCart] = useAtom(writeOnlyAtom);
-
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   function handleClick(e){
-    setAnchorEl(anchorEl ? null :e.currentTarget);
+    setAnchorEl(anchorEl ? null : e.currentTarget);
   }
   
   
@@ -18,8 +21,15 @@ function Cardapio({menus}) {
     setAnchorEl(null);
   };
 
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false)
+  }
+
+  const handleAddToCart = () => {
+    addToCart(anchorEl.innerText)
+    setOpenSnackbar(true)
+    setAnchorEl(null);
+  }
   
   return (
     <>
@@ -53,9 +63,16 @@ function Cardapio({menus}) {
   <Typography sx={{ p: 2 }}>{anchorEl?.innerText}</Typography>
   <div style={{padding: '20px', display: 'flex', gap: '10px'}}>
     <Button variant='contained' onClick={handleClose}color='error'>Cancel</Button>
-    <Button variant='outlined' type='outlined' color='success' onClick={()=>addToCart(anchorEl.innerText)}>Add</Button>
+    <Button variant='outlined' type='outlined' color='success' onClick={handleAddToCart}>Add</Button>
   </div>
-</Popover> 
+  </Popover> 
+    <Snackbar
+    open={openSnackbar}
+    autoHideDuration={6000}
+    onClose={handleCloseSnackbar}
+  >
+    <Alert severity="success" variant="filled">Item added to cart!</Alert>
+  </Snackbar>
     </>
   )
 }
